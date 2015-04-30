e3e59d1021920c03277b48005c465f7ed5a7ed1f
AlphaHack <alpha.hack@outlook.com>
2015-04-30 11:33:14 -0300

    Initial Commit

---
+++ GetIDPS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GetIDPS
+//
+//  Created by Marco Aurelio Viana Almeida on 30/04/15.
+//  Copyright (c) 2015 AlphaHack. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GetIDPSTests/GetIDPSTests.swift
@@ -0,0 +1,36 @@
+//
+//  GetIDPSTests.swift
+//  GetIDPSTests
+//
+//  Created by Marco Aurelio Viana Almeida on 30/04/15.
+//  Copyright (c) 2015 AlphaHack. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class GetIDPSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
